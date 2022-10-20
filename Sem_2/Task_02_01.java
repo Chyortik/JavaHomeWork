e5d4b8de2147359ce2e02d1b1303086f3aef2c9f
Dmitriy <dima4o81@gmail.com>
2022-10-20 22:37:58 +0300

    Update Task_02_01

--- Sem_2/Task_02_01.java
+++ Sem_2/Task_02_01.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class Task_02_01 {
 
     public static void main(String[] args) {
-            System.out.print("Input number of disks: ");
+            System.out.print("Введите количество дисков: ");
             Scanner iScanner = new Scanner(System.in);
             int n = iScanner.nextInt();
             iScanner.close();
         
-            hanoi_tower(n, 'A', 'B', 'C');
+            HanoiTower(n, 'A', 'B', 'C');
     }
 
-       static void hanoi_tower(int n, char start, char middle, char end) {     // n - количество дисков 
+       static void HanoiTower(int n, char start, char middle, char end) {     // n - количество дисков 
         if (n == 1) {                                                          // A, B, C - start, middle, end -башни
             move(n, start, end);
         } else {
-            hanoi_tower(n - 1, start, end, middle);
+            HanoiTower(n - 1, start, end, middle);
             move(n, start, end);
-            hanoi_tower(n - 1, middle, start, end);
+            HanoiTower(n - 1, middle, start, end);
         }
     } 
     public static  void move (int n, char from, char to) {
-        System.out.println(" endure " + n + " from " + from + " to " + to );
+        System.out.println(" переносим " + n + " с " + from + " в " + to );
     }
 
 }
